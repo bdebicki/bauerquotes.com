fb49017ef3410cc50d171b870f4dd412fdf1f097
Earl Debicki <bartosz.debicki@uxpin.com>
2020-06-24 21:24:07 +0200

    handdle condition while same quote has been draw twice

--- src/js/quotesLoader/drawQuote.js
+++ src/js/quotesLoader/drawQuote.js
@@ -1,10 +1,25 @@
 import quotes from '../../../data/quotes.json';
+import getElementByData from '../helpers/getElementByData';
+import { QUOTES_CONTAINER } from '../enums/jsHandlers';
 
-function drawQuote() {
+function drawNewQuoteIndex() {
   const quotesLength = quotes.length;
-  const drawQuoteIndex = Math.floor(Math.random() * (quotesLength));
+  const quotesContainer = getElementByData(QUOTES_CONTAINER);
+  const currentIndex = parseInt(quotesContainer.dataset.index);
+  let newIndex;
 
-  return quotes[drawQuoteIndex];
+  do {
+    newIndex = Math.floor(Math.random() * (quotesLength));
+  } while (currentIndex === newIndex);
+
+  return newIndex;
 }
 
-export default drawQuote;
+function getNewQuote(index) {
+  return quotes[index];
+}
+
+export {
+  drawNewQuoteIndex,
+  getNewQuote,
+};
--- src/js/quotesLoader/updateQuote.js
+++ src/js/quotesLoader/updateQuote.js
@@ -1,11 +1,13 @@
 import getElementByData from '../helpers/getElementByData';
 import { QUOTES_CONTAINER } from '../enums/jsHandlers';
-import drawQuote from './drawQuote';
+import { drawNewQuoteIndex, getNewQuote } from './drawQuote';
 
 function updateQuote() {
   const quotesContainer = getElementByData(QUOTES_CONTAINER);
+  const newQuoteIndex = drawNewQuoteIndex();
 
-  quotesContainer.textContent = drawQuote();
+  quotesContainer.dataset.index = newQuoteIndex;
+  quotesContainer.textContent = getNewQuote(newQuoteIndex);
 }
 
 export default updateQuote;
